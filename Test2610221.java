e25397ed838d1a80923a9fc04cab9c28b9449fcb
resunrichard <116341273+resunrichard@users.noreply.github.com>
2022-11-11 13:53:24 +0530

    Add files via upload

---
+++ Test2610221.java
@@ -0,0 +1,9 @@
+class Test2610221{
+	public static void main(String args[]){
+		System.out.println("List of 100 natural numbers");
+		for(int i=1;i<=100;i++)
+		{
+		System.out.print(" "+i);
+		}
+	}
+}
